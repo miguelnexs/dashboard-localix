21dad0caa232f043b09bd3333a796dfbd69ba3cb
Miguel <miguel@example.com>
2025-11-14 11:29:02 -0500

    Cambio de base de datos

--- backend/globetrek_backend/settings.py
+++ backend/globetrek_backend/settings.py
@@ -95,15 +95,8 @@ WSGI_APPLICATION = 'globetrek_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'options': '-c client_encoding=UTF8'
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
